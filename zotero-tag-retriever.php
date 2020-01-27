32172b97c5405c5e4c1d9b8cc0ddd2904a621ea8
ummon-ai <60343576+ummon-ai@users.noreply.github.com>
2020-01-27 15:54:25 +0200

    Update zotero-tag-retriever.php

--- zotero-tag-retriever.php
+++ zotero-tag-retriever.php
@@ -60,69 +60,6 @@ else {
 return;
 ?>
 
-
-<?php
-//* Script : Zotero API tag retriever 		*//
-//* Description:					  	*//
-//* Get the tags for your Zotero collection  *//
-//* using the Zotero API. Print them out in	*//
-//* a webpage (Wordpress etc.).    	     *//
-//* Author: Aatu Poutanen.  		  		*//
-//* Free to use and modify. 		   		*//
-
-// Define variables.
-
-$StartFrom = 0; 
-$NumTags = 70;
-$x=0;
-
-/* Define API request*/
-/* Run the script x times. */
-while ($x < 3) {
-		$request = wp_remote_get( "https://api.zotero.org/users/XXXXXX/collections/XXXXXX/tags?&start=" . $StartFrom . "&limit=" . $NumTags); // your collection API here.
-			$body = wp_remote_retrieve_body( $request );
-			$data[] = json_decode( $body );
-		if( is_wp_error( $request ) ) {
-		echo 'Unable to retrieve tags from Zotero API';
-		return;
-		}	
-	$StartFrom += 70;
-	$x++;
-
-}
-
-// Extract tags to a dedicated array.
-
-if( isset( $data ) ){
-		foreach( $data as $key => $value ) {
-			foreach( $value as $sub_key => $sub_value ) {
-				$tags[] = $sub_value->tag;
-			}
-		}
-}
-
-// Organize tags by name.
-
-function cmp($a, $b) {
-    return strcmp($a["tag"], $b["tag"]);
-}
-usort($tags, "cmp");
-
-// /*print tags on page, convert to links */
-
-if( ! empty( $tags ) ) {
-	foreach( $tags as $key => $value ) {
-		echo '<div class="tagbutton">' . '<a href="your-wp-website-here/?tag_id=' . $value . '">' . $value . " " . '</a>'. '</div>';
-			}
-}
-else {
-	echo "Object has no data." . '<br>';
-	return;
-}
-return;
-?>
-
-
 <!-- CSS to style your tags into Zotero -style buttons. Add this to your child theme or as custom CSS in WordPress.
 	
 .tagbutton {
